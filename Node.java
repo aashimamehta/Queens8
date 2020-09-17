0e72e092ce200764da457b65771c88f12bd74f38
Aashima Mehta <58320925+aashimamehta@users.noreply.github.com>
2020-09-17 14:38:36 -0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,19 +1,7 @@
-
-
-
-
-
-
-
-/*
-  public class Node {
+public class Node {
      public Node left;
      public Node right;
      public int val;
  }
 
-public Node findAncestor(Node root, Node p, Node q) {
-    // your code here
-}
 
-*/
